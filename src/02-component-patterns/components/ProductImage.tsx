cf28e3a473c77b689d03b7f7c67a5ed66c3e7fbd
MaVarela <marianovarela89@yahoo.es>
2023-01-09 18:14:51 -0300

    Component extend styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,9 +1,15 @@
-import { useContext } from "react";
-import { ProductContext } from "./ProductCard";
+import { CSSProperties, useContext } from 'react';
+import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductImage = ({ img = '' }) => {
+export interface Props {
+  img?: string;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductImage = ({ img = '', className, style } : Props) => {
   const { product } = useContext(ProductContext);
   let imgToShow: string;
 
@@ -16,6 +22,11 @@ export const ProductImage = ({ img = '' }) => {
   }
 
   return (
-    <img className={styles.productImg} src={imgToShow} alt="Product image" />
+    <img
+      className={`${styles.productImg} ${className}`}
+      src={imgToShow}
+      style={style}
+      alt="Product image"
+    />
   );
 };
--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -1,10 +1,7 @@
-import exp from "constants";
-import { ReactElement } from "react";
-
-export interface ProductCardProps {
-  product: Product;
-  children?: ReactElement | ReactElement[];
-};
+import { Props as ProductCardProps } from '../components/ProductCard';
+import { Props as ProductTitleProps } from '../components/ProductTitle';
+import { Props as ProductImageProps } from '../components/ProductImage';
+import { Props as ProductButtonsProps } from '../components/ProductButtons';
 
 export interface Product {
   id: string;
@@ -20,7 +17,7 @@ export interface ProductContextProps {
 
 export interface ProductCardHOCProps {
   ({ children, product }: ProductCardProps): JSX.Element;
-  Title: ({ title }: { title?: string | undefined; }) => JSX.Element;
-  Image: ({ img }: { img?: string | undefined; }) => JSX.Element;
-  Buttons: () => JSX.Element;
+  Title: ({ title, className }: ProductTitleProps) => JSX.Element;
+  Image: ({ img, className }: ProductImageProps) => JSX.Element;
+  Buttons: ({ className }: ProductButtonsProps) => JSX.Element;
 };
