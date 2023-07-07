424b551288c929a1e7221ba3870c7d76fe285c25
MaVarela <marianovarela89@yahoo.es>
2023-07-07 12:23:48 -0300

    cambios para preparar lib

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,5 @@
 import { ProductCard, ProductImage, ProductButtons, ProductTitle } from "../components";
-
 import { products } from "../data/products";
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -14,22 +12,17 @@ export const ShoppingPage = () => {
         <ProductCard
           key={product.id}
           product={product}
-          className="bg-dark text-white"
           initialValues={{
             count: 4,
-            maxCount: 10,
+            // maxCount: 10,
           }}
         >
           {
             ({ reset, isMaxCountReached, count, increaseBy }) => (
               <>
-                <ProductImage className="custom-image" />
-                <ProductTitle className="text-bold" />
-                <ProductButtons className="custom-buttons" />
-                <button onClick={reset}>Reset</button>
-                <button onClick={()=> increaseBy(-2)}>-2</button>
-                {!isMaxCountReached && (<button onClick={()=> increaseBy(2)}>+2</button>)}
-                <span>{count}</span>
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons />
               </>
             )
           }
